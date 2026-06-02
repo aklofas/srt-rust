b71f432288cba652993c8aeb4829358174de0976
Andrew Klofas <aklofas@gmail.com>
2026-06-02 00:37:23 -0700

    freertos-srt: move shared substrate (FreeRTOS/lwIP/libsrt glue, drivers, patches)

---
+++ embedded/s3-srt/srt-build1/CMakeFiles/srt_virtual.dir/haicrypt/haicrypt_log.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/srt_virtual.dir/haicrypt/haicrypt_log.cpp.obj: \
+ /home/aklofas/Projects/ts-transformer/ts-transformer/vendor/srt/haicrypt/haicrypt_log.cpp \
+ /home/aklofas/Projects/ts-transformer/ts-transformer/embedded/s3-srt/posix-shims/s2_prefix.h
